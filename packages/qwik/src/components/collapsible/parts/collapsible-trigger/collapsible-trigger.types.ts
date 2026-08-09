7ad483d5c5940544aa10fab31f1ab4e2b58e7e30
ZAHON <konrad.zahon@gmail.com>
2026-08-09 14:51:56 +0200

    docs(packages/qwik): add angle brackets to `Collapsible.Trigger` in `CollapsibleTriggerProps` interface JSDoc
    Wrap `Collapsible.Trigger` in angle brackets within the `CollapsibleTriggerProps` interface JSDoc description for consistency.

--- packages/qwik/src/components/collapsible/parts/collapsible-trigger/collapsible-trigger.types.ts
+++ packages/qwik/src/components/collapsible/parts/collapsible-trigger/collapsible-trigger.types.ts
@@ -1,7 +1,7 @@
 import { PropsOf, Component } from '@qwik.dev/core';
 
 /**
- * Props for the `Collapsible.Trigger` component.
+ * Props for the `<Collapsible.Trigger>` component.
  *
  * Extends the standard HTML attributes for a `<button>` element.
  */
