8bd8a5c463d16ccd13b4f6af80bc41ccb99ca130
ZAHON <konrad.zahon@gmail.com>
2026-08-09 11:32:40 +0200

    docs(packages/qwik): add angle brackets to `Dialog.Trigger` in `DialogTriggerProps` interface JSDoc
    Wrap `Dialog.Trigger` in angle brackets within the `DialogTriggerProps` interface JSDoc description for consistency.

--- packages/qwik/src/components/dialog/parts/dialog-trigger/dialog-trigger.types.ts
+++ packages/qwik/src/components/dialog/parts/dialog-trigger/dialog-trigger.types.ts
@@ -1,7 +1,7 @@
 import { PropsOf, Component } from '@qwik.dev/core';
 
 /**
- * Props for the `Dialog.Trigger` component.
+ * Props for the `<Dialog.Trigger>` component.
  *
  * Extends the standard HTML attributes for a `<button>` element.
  */
