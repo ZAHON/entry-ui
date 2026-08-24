2d9500c303ed4e0b0906340a29ab23f3a4f7ab0c
ZAHON <konrad.zahon@gmail.com>
2026-08-24 22:12:08 +0200

    docs(packages/utilities): expand JSDoc line width in `getWindow` utility
    Reformat the JSDoc description for `getWindow` to increase line length, improving text wrapping and readability in modern code editors.

--- packages/utilities/src/dom/get-window/get-window.ts
+++ packages/utilities/src/dom/get-window/get-window.ts
@@ -1,10 +1,10 @@
 /**
  * Retrieves the window object associated with a given DOM node, ensuring the correct execution context.
  *
- * This utility identifies the correct execution context (global window) for a specific
- * node. It is particularly useful when working with applications that utilize iframes
- * or multiple windows, as it ensures that window-level APIs and properties are
- * accessed from the node's owner document rather than the current top-level window.
+ * This utility identifies the correct execution context (global window) for a specific node.
+ * It is particularly useful when working with applications that utilize iframes or multiple windows,
+ * as it ensures that window-level APIs and properties are accessed from the node's owner document
+ * rather than the current top-level window.
  *
  * @example
  * ```ts
