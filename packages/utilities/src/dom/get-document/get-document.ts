3a49140cd9ca005c54e635516abe5705fddc2618
ZAHON <konrad.zahon@gmail.com>
2026-08-24 21:48:56 +0200

    refactor(packages/utilities): remove explicit `Document` return type from `getDocument` utility
    Remove the redundant `: Document` return type annotation from the `getDocument` utility, relying on TypeScript's automatic return type inference.

--- packages/utilities/src/dom/get-document/get-document.ts
+++ packages/utilities/src/dom/get-document/get-document.ts
@@ -14,7 +14,7 @@
  * const doc = getDocument(element);
  * ```
  */
-export const getDocument = (node: Node | null | undefined): Document => {
+export const getDocument = (node: Node | null | undefined) => {
   // Access the `ownerDocument` property to retrieve the document instance where the `node` lives.
   // If the `node` is `null`, `undefined`, or detached from a specific context, fallback to the global `document`.
   return node?.ownerDocument || document;
