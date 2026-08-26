b12257ebbdf3f4e752b92d2dd7902f3b8ad92398
ZAHON <konrad.zahon@gmail.com>
2026-08-26 22:54:41 +0200

    feat(packages/utilities)!: remove `setStyle` utility
    Remove the `setStyle` utility function, its TypeScript interface definitions, unit tests, and associated documentation.
    
    - Remove `setStyle` implementation and `SetStyleParams` interface.
    - Remove `setStyle` exports from package index.
    - Delete `setStyle` unit tests.
    - Delete `setStyle` documentation file.
    
    The `setStyle` utility and `SetStyleParams` interface have been completely removed from `@entry-ui/utilities`.

--- packages/utilities/src/style/index.ts
+++ packages/utilities/src/style/index.ts
@@ -2,7 +2,6 @@ export * from './get-computed-style';
 export * from './get-css-dimensions';
 export * from './get-hidden-element-height';
 export * from './merge-styles';
-export * from './set-style';
 export * from './set-style-property';
 export * from './visually-hidden-input-style';
 export * from './visually-hidden-style';
